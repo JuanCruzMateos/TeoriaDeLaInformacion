011cf79ec225995a2d6236c87cda89e73f169a2a
JuanCruzMateos <juanczmt@gmail.com>
2021-09-22 02:11:45 -0300

    going

--- EntregaUno/src/main/java/incisoa/FileDecoder.java
+++ EntregaUno/src/main/java/incisoa/FileDecoder.java
@@ -1,20 +1,27 @@
 package incisoa;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.TreeSet;
 
-public class FileParser {
+public class FileDecoder {
     private final HashMap<String, Integer> frec = new HashMap<>();
     private final HashMap<String, Double> prob = new HashMap<>();
     private final HashMap<String, Double> cantInfo = new HashMap<>();
     private TreeSet<String> palabras;
 
+    public static void main(String[] args) throws IOException {
+        FileDecoder fd = new FileDecoder();
+
+        fd.probCondicionales("src/resources/anexo1-grupo5.txt");
+    }
     public void parseFile(String inputfile, int digitosPalabra) throws IOException {
         Reader reader = new FileReader(inputfile);
         char[] buffer = new char[digitosPalabra];
         String word;
         int total;
+        double prob;
 
         while (reader.read(buffer) != -1) {
             word = new String(buffer);
@@ -26,8 +33,9 @@ public class FileParser {
         total = this.frec.values().stream().mapToInt(Integer::intValue).sum();
 
         for (String str : this.palabras) {
-            this.prob.put(str, (double) this.frec.get(str) / total);
-            this.cantInfo.put(str, cantidadDeInformacion(this.prob.get(str)));
+            prob = (double) this.frec.get(str) / total;
+            this.prob.put(str, prob);
+            this.cantInfo.put(str, cantidadDeInformacion(prob));
         }
     }
 
@@ -36,6 +44,7 @@ public class FileParser {
         for (String s : this.palabras) {
             System.out.println("[" + s + "] = " + this.frec.get(s));
         }
+        System.out.println("total = " + this.frec.values().stream().mapToInt(Integer::intValue).sum());
         System.out.println();
     }
 
@@ -77,19 +86,21 @@ public class FileParser {
         this.printCantidadDeInfo();
         System.out.println("Entropia:");
         System.out.println("H(S) = " + this.entropia());
-        System.out.println();
         System.setOut(stdout);
     }
 
     public void writeToCsv(String outputfile) throws IOException {
         Writer file = new FileWriter(outputfile);
+        int total = this.frec.values().stream().mapToInt(Integer::intValue).sum();
+        double suma = this.prob.values().stream().mapToDouble(Double::doubleValue).sum();
 
         file.write(String.join(",", "Palabras", "Frecuencia", "Probabilidad", "Cant. Informacion", "\n"));
         for (String key : this.palabras) {
             file.write(String.join(",", key, Integer.toString(this.frec.get(key)), Double.toString(this.prob.get(key)),
                     Double.toString(this.cantInfo.get(key)), "\n"));
         }
-        file.write(String.join(",", "Entropia", Double.toString(this.entropia())));
+        file.write(String.join(",", " ", Integer.toString(total), Double.toString(suma), " \n"));
+        file.write(String.join(",", "Entropia H(S)", Double.toString(this.entropia()), "\n"));
         file.close();
     }
 
@@ -98,5 +109,40 @@ public class FileParser {
         this.prob.clear();
         this.frec.clear();
         this.cantInfo.clear();
+    }
+
+    private int toDec(int bin) {
+        int dec = 0, i = 1;
+        int nro;
+
+        do {
+            nro = bin % 10;
+            dec += nro * i;
+            i *= 2;
+            bin = bin / 10;
+        } while (bin != 0);
+        return dec;
+    }
+
+    public void probCondicionales(String inputfile) throws IOException {
+        double[][] probs = new double[4][4];
+        Reader reader = new FileReader(inputfile);
+        char[] buffer = new char[2];
+        int ant, act;
+
+        if (reader.read(buffer) == -1) {
+            throw new IOException("Eof");
+        }
+        ant = this.toDec(Integer.parseInt(new String(buffer)));
+        while (reader.read(buffer) != -1) {
+            act = this.toDec(Integer.parseInt(new String(buffer)));
+            probs[act][ant] += 1;
+            ant = act;
+        }
+        reader.close();
+
+        System.out.println(Arrays.deepToString(probs));
+
+        
     }
 }
--- EntregaUno/src/main/java/incisoa/Main.java
+++ EntregaUno/src/main/java/incisoa/Main.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        FileDecoder fileDecoder = new FileDecoder();
         try {
-            FileParser fileParser = new FileParser();
             for (int i = 5; i < 10; i += 2) {
-                fileParser.parseFile("src/resources/anexo1-grupo5.txt", i);
-//            fileParser.printFrecuencies();
-//            fileParser.printProbabilidad();
-//            fileParser.printCantidadDeInfo();
-//            System.out.println("H(S) = " + fileParser.entropia());
-                fileParser.writeToTxt("src/results/resultados" + i + "digitos.txt");
-                fileParser.writeToCsv("src/results/resultados" + i + "digitos.csv");
-                fileParser.clearAll();
+                fileDecoder.parseFile("src/resources/anexo1-grupo5.txt", i);
+//            fileDecoder.printFrecuencies();
+//            fileDecoder.printProbabilidad();
+//            fileDecoder.printCantidadDeInfo();
+//            System.out.println("H(S) = " + fileDecoder.entropia());
+                fileDecoder.writeToTxt("src/results/resultados" + i + "digitos.txt");
+                fileDecoder.writeToCsv("src/results/resultados" + i + "digitos.csv");
+                fileDecoder.clearAll();
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
