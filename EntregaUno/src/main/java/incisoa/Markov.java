627823ca2bbf122e8918da78e889a709f3e85356
JuanCruzMateos <juanczmt@gmail.com>
2021-09-23 01:29:33 -0300

    markov

--- EntregaUno/src/main/java/incisoa/Markov.java
+++ EntregaUno/src/main/java/incisoa/Markov.java
@@ -1,16 +1,76 @@
 package incisoa;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.Arrays;
 
 public class Markov {
+    private static final int DIGITOS = 2;
+    private static final int ESTADOS = 4;
+    private final double[][] probCond = new double[ESTADOS][ESTADOS];
+    private final double[] vecEstacionario = new double[ESTADOS];
 
     public static void main(String[] args) throws IOException {
         Markov markov = new Markov();
 
         markov.probCondicionales("src/resources/anexo1-grupo5.txt");
+        markov.calcVectorEstacionario();
+        markov.writeTxt("src/results/markov.txt");
+    }
+
+    public void probCondicionales(String inputfile) throws IOException {
+        Reader reader = new FileReader(inputfile);
+        char[] buffer = new char[DIGITOS];
+        int ant, act;
+
+        if (reader.read(buffer) == -1) {
+            throw new IOException("Eof");
+        }
+        ant = this.toDec(Integer.parseInt(new String(buffer)));
+        while (reader.read(buffer) != -1) {
+            act = this.toDec(Integer.parseInt(new String(buffer)));
+            this.probCond[act][ant] += 1;
+            ant = act;
+        }
+        reader.close();
+        for (int j = 0; j < ESTADOS; j++) {
+            int finalJ = j;
+            double sum = Arrays.stream(this.probCond).mapToDouble(row -> row[finalJ]).sum();
+            for (int i = 0; i < 4; i++) {
+                this.probCond[i][j] /= sum;
+            }
+        }
+    }
+
+    public void calcVectorEstacionario() {
+        double[] iter = new double[ESTADOS];
+        double[] aux = new double[ESTADOS];
+        double tol = 10E-7;
+        double err = 1;
+        double acum;
+
+        this.vecEstacionario[0] = 1;
+        for (int i = 1; i < ESTADOS; i++) {
+            this.vecEstacionario[i] = 0;
+        }
+
+        while (err > tol) {
+            for (int i = 0; i < ESTADOS; i++) {
+                acum = 0;
+                for (int j = 0; j < ESTADOS; j++)
+                    acum += this.probCond[i][j] * this.vecEstacionario[j];
+                iter[i] = acum;
+            }
+            for (int i = 0; i < ESTADOS; i++) {
+                aux[i] = iter[i] - this.vecEstacionario[i];
+                this.vecEstacionario[i] = iter[i];
+            }
+            err = this.normaM(aux);
+        }
+    }
+
+    private double normaM(double[] vec) {
+        double[] p = Arrays.stream(vec).map(i -> i > 0 ? i : i * -1).toArray();
+        return Arrays.stream(p).max().getAsDouble();
     }
 
     private int toDec(int bin) {
@@ -24,45 +84,32 @@ public class Markov {
         return dec;
     }
 
-    public void probCondicionales(String inputfile) throws IOException {
-        double[][] probs = new double[4][4];
-        Reader reader = new FileReader(inputfile);
-        char[] buffer = new char[2];
-        int ant, act;
-
-        if (reader.read(buffer) == -1) {
-            throw new IOException("Eof");
-        }
-        ant = this.toDec(Integer.parseInt(new String(buffer)));
-        while (reader.read(buffer) != -1) {
-            act = this.toDec(Integer.parseInt(new String(buffer)));
-            probs[act][ant] += 1;
-            ant = act;
-        }
-        reader.close();
-
-        this.printMat(probs);
-        for (int j = 0; j < 4; j++) {
-            int finalJ = j;
-            double sum = Arrays.stream(probs).mapToDouble(row -> row[finalJ]).sum();
-//            System.out.println(finalJ + " " + sum);
-            for (int i = 0; i < 4; i++) {
-                probs[i][j] /= sum;
-            }
-        }
-        this.printMat(probs);
-    }
-
-    private void printMat(double[][] mat) {
-        for (int i = 0; i < 4; i++) {
+    public void printProbCond() {
+        System.out.println("Probabilidades condicionales: ");
+        for (int i = 0; i < ESTADOS; i++) {
             System.out.print('|');
-            for (int j = 0; j < 4; j++) {
-                System.out.printf("%10.2f", mat[i][j]);
+            for (int j = 0; j < ESTADOS; j++) {
+                System.out.printf("%10.6f", this.probCond[i][j]);
             }
             System.out.println("|");
         }
     }
 
+    public void printVectorEstacionario() {
+        System.out.println("Vector estacionario: ");
+        System.out.println(Arrays.toString(this.vecEstacionario));
+    }
+
+    public void writeTxt(String filename) throws FileNotFoundException {
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(filename));
+        this.printProbCond();
+        System.out.println();
+        this.printVectorEstacionario();
+        System.setOut(stdout);
+    }
+
+    // TODO: 23/09/2021  
     private boolean warshall(double[][] probs) {
         int[][] a = new int[4][4];
         int i, j, k;
