bb8efff3b6391683657dc2d5f076ad32a74e2f0a
JuanCruzMateos <juanczmt@gmail.com>
2021-09-23 13:03:41 -0300

    vec est

--- EntregaUno/src/main/java/incisoa/Markov.java
+++ EntregaUno/src/main/java/incisoa/Markov.java
@@ -4,18 +4,12 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Markov {
+    private static final double TOL = 10E-7;
     private static final int INF = 999;
     private static final int DIGITOS = 2;
     private static final int ESTADOS = 4;
-    private final double[][] probCond = new double[ESTADOS][ESTADOS];
-    private final double[] vecEstacionario = new double[ESTADOS];
-
-    public Markov() {
-        this.vecEstacionario[0] = 1;
-        for (int i = 1; i < ESTADOS; i++) {
-            this.vecEstacionario[i] = 0;
-        }
-    }
+    private final double[][] probCond = new double[Markov.ESTADOS][Markov.ESTADOS];
+    private final double[] vecEstacionario = new double[Markov.ESTADOS];
 
     public static void main(String[] args) {
         Markov markov = new Markov();
@@ -30,14 +24,16 @@ public class Markov {
         }
     }
 
+
     public void readFile(String inputfile) throws IOException {
         Reader reader = new FileReader(inputfile);
-        char[] buffer = new char[DIGITOS];
+        char[] buffer = new char[Markov.DIGITOS];
         int ant, act;
 
         if (reader.read(buffer) == -1) {
             throw new IOException("Eof");
         }
+        // TODO :: revisar
         ant = this.toDec(Integer.parseInt(new String(buffer)));
         while (reader.read(buffer) != -1) {
             act = this.toDec(Integer.parseInt(new String(buffer)));
@@ -48,7 +44,7 @@ public class Markov {
     }
 
     public void calcProbCondicionales() {
-        for (int j = 0; j < ESTADOS; j++) {
+        for (int j = 0; j < Markov.ESTADOS; j++) {
             int finalJ = j;
             double sum = Arrays.stream(this.probCond).mapToDouble(row -> row[finalJ]).sum();
             for (int i = 0; i < 4; i++) {
@@ -58,30 +54,50 @@ public class Markov {
     }
 
     public void calcVectorEstacionario() {
-        double[] iter = new double[ESTADOS];
-        double[] aux = new double[ESTADOS];
-        double tol = 10E-7;
-        double err = 1;
-        double acum;
+        double[] iter = new double[Markov.ESTADOS];
+        double err = Markov.TOL;
 
-        while (err > tol) {
-            for (int i = 0; i < ESTADOS; i++) {
-                acum = 0;
-                for (int j = 0; j < ESTADOS; j++)
-                    acum += this.probCond[i][j] * this.vecEstacionario[j];
-                iter[i] = acum;
+        // inicializacion
+        this.vecEstacionario[0] = 1;
+        for (int i = 1; i < ESTADOS; i++) {
+            this.vecEstacionario[i] = 0;
+        }
+        // calculo iterativo
+        while (err >= Markov.TOL) {
+            for (int i = 0; i < Markov.ESTADOS; i++) {
+                iter[i] = 0;
+                for (int j = 0; j < Markov.ESTADOS; j++)
+                    iter[i] += this.probCond[i][j] * this.vecEstacionario[j];
             }
-            for (int i = 0; i < ESTADOS; i++) {
-                aux[i] = iter[i] - this.vecEstacionario[i];
-                this.vecEstacionario[i] = iter[i];
-            }
-            err = this.normaM(aux);
+            err = normaM(iter, this.vecEstacionario);
+            System.arraycopy(iter, 0, this.vecEstacionario, 0, Markov.ESTADOS);
         }
     }
 
-    private double normaM(double[] vec) {
-        double[] p = Arrays.stream(vec).map(i -> i > 0 ? i : i * -1).toArray();
-        return Arrays.stream(p).max().getAsDouble();
+    public double normaM(double[] act, double[] ant) {
+        double max, diff;
+
+        max = Math.abs(act[0] - ant[0]);
+        for (int i = 1; i < act.length; i++) {
+            diff = Math.abs(act[i] - ant[i]);
+            if (diff > max)
+                max = diff;
+        }
+        return max;
+    }
+
+    public double entropia(double[][] prob, double[] vec) {
+        double h = 0, aux;
+
+        for (int i = 0; i < vec.length; i++) {
+            aux = 0;
+            for (int j = 0; j < vec.length; j++) {
+                if (prob[j][i] != 0)
+                    aux += prob[j][i] * -1. * Math.log(prob[j][i]) / Math.log(2);
+            }
+            h += aux * vec[i];
+        }
+        return h;
     }
 
     private int toDec(int bin) {
@@ -97,9 +113,9 @@ public class Markov {
 
     public void printProbCond() {
         System.out.println("Probabilidades condicionales: ");
-        for (int i = 0; i < ESTADOS; i++) {
+        for (int i = 0; i < Markov.ESTADOS; i++) {
             System.out.print('|');
-            for (int j = 0; j < ESTADOS; j++) {
+            for (int j = 0; j < Markov.ESTADOS; j++) {
                 System.out.printf("%10.6f", this.probCond[i][j]);
             }
             System.out.println("|");
@@ -117,6 +133,8 @@ public class Markov {
         this.printProbCond();
         System.out.println();
         this.printVectorEstacionario();
+        System.out.println();
+        System.out.println("Entropia:\nH(S) = " + this.entropia(this.probCond, this.vecEstacionario));
         System.setOut(stdout);
     }
 
@@ -145,11 +163,7 @@ public class Markov {
                 }
             }
         }
-        return false;
-    }
-
-    public double entropia() {
-        return 0;
+        return true;
     }
 
 }
