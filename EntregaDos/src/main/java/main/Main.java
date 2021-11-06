7745fac6da3167427c06ef7ad3ef790fb92ec707
JuanCruzMateos <juanczmt@gmail.com>
2021-11-06 12:04:56 -0300

    ref

--- EntregaDos/src/main/java/main/Main.java
+++ EntregaDos/src/main/java/main/Main.java
@@ -1,42 +1,30 @@
 package main;
 
 import modelo.compresion.Huffman;
+import modelo.compresion.RLC;
+import modelo.compresion.ShannonFano;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String file1 = "Argentina.txt";
-        String file2 = "Hawai.txt";
-        String file3 = "imagen.raw";
-
+        String[] files = {"Argentina.txt", "Hawai.txt", "imagen.raw"};
         Huffman huffman = new Huffman();
-        try {
-            huffman.parseFile(file1, 1);
-            huffman.crearArbolHuffman();
-            huffman.generarCodigos();
-            huffman.writeHuffmanToTxt("ArgentinaHuf.txt");
-            huffman.compress();
-            huffman.decompress();
-            huffman.clearAll();
-
-            huffman.parseFile(file2, 1);
-            huffman.crearArbolHuffman();
-            huffman.generarCodigos();
-            huffman.writeHuffmanToTxt("HawaiHuf.txt");
-            huffman.compress();
-            huffman.decompress();
-            huffman.clearAll();
-
-            huffman.parseFile(file3, 1);
-            huffman.crearArbolHuffman();
-            huffman.generarCodigos();
-            huffman.writeHuffmanToTxt("imagenHuf.txt");
-            huffman.compress();
-            huffman.decompress();
-            huffman.clearAll();
-        } catch (IOException e) {
-            e.printStackTrace();
+        ShannonFano shannonFano = new ShannonFano();
+        RLC rlc = new RLC();
+        
+        for (String file : files) {
+            try {
+                huffman.parseFile(file, 1);
+                huffman.crearArbolHuffman();
+                huffman.generarCodigos();
+                huffman.writeHuffmanToTxt("ArgentinaHuf.txt");
+                huffman.compress();
+                huffman.decompress();
+                huffman.clearAll();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
